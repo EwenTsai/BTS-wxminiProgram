9b683a9e4fdac9edd5baa57a8916cbcd39284c30
ewentsai <ewen.chua.gi@gmail.com>
2020-03-17 14:21:12 +0800

    完成书本详情页面开发

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,9 +5,60 @@ Page({
    * 页面的初始数据
    */
   data: {
+    js_code: '',
+    isLogin:false,
 
   },
 
+  onclick: function (e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.userInfo)
+    console.log(e.detail.rawData)
+    // var that = this
+    // wx.login({
+    //   success: function (res) {
+    //     if(res.code){
+    //       that.js_code = res.code
+    //       wx.getUserInfo({
+    //         success:function (infoRes){
+    //           wx.request({
+    //             url: 'http://localhost:8080/BTS/api/user/wxLogin',
+    //             data: {
+    //               code: that.js_code
+    //             },
+    //             success: function (res) {
+    //               console.log('login success');
+    //               res = res.data;
+    //               if (res.result == 0) {
+    //                 that.globalData.userInfo = res.userInfo;
+    //                 wx.setStorageSync('userInfo', JSON.stringify(res.userInfo));
+    //                 wx.setStorageSync('loginFlag', res.skey);
+    //                 console.log("skey=" + res.skey);
+    //                 callback();
+    //               }
+    //             }
+    //           })
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
+
+    // wx.login({
+    //   success: function(res) {
+    //     if(res.code){
+    //       that.getSessionKey(res.code, "wxaf2296592f00e75c", "5efdbb85521eed60f1a606d14b1b90f9")
+    //     }
+    //     console.log(res)
+    //   },
+    //   fail: function(res) {
+    //     console.log(res)
+    //   },
+    //   complete: function(res) {
+    //     console.log(res)
+    //   },
+    // })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
