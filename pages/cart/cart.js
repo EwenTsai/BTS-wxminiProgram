55fe1bf58be6749b059bd73b23aaa80b7a9179fe
ewentsai <ewen.chua.gi@gmail.com>
2020-03-25 21:32:37 +0800

    完成购物车页面开发

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -7,6 +7,7 @@ Page({
   data: {
     isNull: false,
     books: [],
+    amount: 0
   },
 
   /**
@@ -17,18 +18,25 @@ Page({
     wx.request({
 
       url: "http://localhost:8080/BTS/api/Cart",
+      data:{
+        uid: wx.getStorageSync('uid')
+      },
 
       success: function (res) {
-        console.log(res.data)
         if(res.data.data.length==0){
           that.setData({
             isNull: true
           })
+        }else{
+          that.setData({
+            books: res.data.data
+          })
+          for (var i = 0; i < res.data.data.length; i++) {
+            that.setData({
+              amount: that.data.amount + res.data.data[i].price * res.data.data[i].amount
+            })
+          }
         }
-        that.setData({
-          books: res.data
-        })
-        console.log(res.data.length)
       },
       fail: function (err) {
         console.log(err)
